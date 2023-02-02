7a03f32e84aa411fffe19fdfaa0a66bb39b9e137
Sachetto <lucas.sachetto84@gmail.com>
2023-02-02 11:06:34 -0300

    Colocando try e catch para exceções

--- pilha.php
+++ pilha.php
@@ -2,13 +2,21 @@
 
 function funcao1(){
     echo "Entrei na função 1" . PHP_EOL;
-    funcao2();
+    try {
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $problema) {
+        echo "Resolvi o problema na função 1" . PHP_EOL;
+    }
     echo "Saindo da função 1" . PHP_EOL;
 
 }
 
 function funcao2(){
     echo "Entrei na função 2" . PHP_EOL;
+
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = "Valor";
+    intdiv(8, 0);
 
     for ($i = 1; $i <= 5; $i++){
         echo $i . PHP_EOL;
